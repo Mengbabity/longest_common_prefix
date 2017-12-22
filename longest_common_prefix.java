fcdf7e273aeace4faa9573ff630da4ca4feb6f1b
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2017-12-22 17:52:14 +0800

    Create longest_common_prefix.java

---
+++ longest_common_prefix.java
@@ -0,0 +1,11 @@
+package longest_common_prefix;
+
+public class longest_common_prefix {
+	public static void main(String[] args)
+	{
+		solution s=new solution();
+		String[] strs={"carp","carpet","carpture"};
+		s.longestCommonPrefix(strs);
+	}
+
+}
